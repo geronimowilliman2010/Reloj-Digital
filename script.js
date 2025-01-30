e88f2fbb63836e62655181ea9fae1700fdc5c022
geronimowilliman2010 <geronimowiliman@gmail.com>
2025-01-30 03:53:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
-function actualizarReloj() {
-    const horaElement = document.getElementById('hora');
-    
-    // Obtener la hora actual
-    const fecha = new Date();
-    let horas = fecha.getHours();
-    let minutos = fecha.getMinutes();
-    let segundos = fecha.getSeconds();
+function actualizarHora() {
+    const ahora = new Date();
+    let horas = ahora.getHours();
+    let minutos = ahora.getMinutes();
+    let segundos = ahora.getSeconds();
 
-    // Asegurarse de que los valores sean siempre de 2 dígitos (por ejemplo, 09 en lugar de 9)
     horas = horas < 10 ? '0' + horas : horas;
     minutos = minutos < 10 ? '0' + minutos : minutos;
     segundos = segundos < 10 ? '0' + segundos : segundos;
 
-    // Mostrar la hora en el elemento HTML
-    horaElement.textContent = `${horas}:${minutos}:${segundos}`;
+    const tiempo = `${horas}:${minutos}:${segundos}`;
+    document.getElementById('reloj').innerHTML = tiempo;
 }
 
-// Actualizar el reloj cada segundo
-setInterval(actualizarReloj, 1000);
-
-// Llamar una vez para mostrar la hora de inmediato
-actualizarReloj();
+setInterval(actualizarHora, 1000);
